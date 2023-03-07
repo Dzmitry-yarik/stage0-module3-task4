ab4c8228000eeaf352a7411a5e4078149d1313f6
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-07 17:16:27 +0300

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+         int q = 99;
+        int w = 104;
+        int e = 97;
+        int r = 114;
+        char z = (char) q;
+        char x = (char) w;
+        char c = (char) e;
+        char v = (char) r;
+        System.out.println(z);
+        System.out.println(x);
+        System.out.println(c);
+        System.out.println(v);
     }
 }
